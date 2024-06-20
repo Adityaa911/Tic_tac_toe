2b2dea75cb13c972d22a8403db3daef86b52f675
Adityaa911 <dukeaddy9@gmail.com>
2024-06-20 14:35:30 +0530

    Update ColWinningStrategy.java

--- src/main/java/org/example/Strategies/WinningStrategy/ColWinningStrategy.java
+++ src/main/java/org/example/Strategies/WinningStrategy/ColWinningStrategy.java
@@ -1,4 +1,27 @@
 package org.example.Strategies.WinningStrategy;
 
 public class ColWinningStrategy implements WinningStrategy{
+  HashMap<Integer,Map<Symbol,Integer>> colCounts =new HashMap<>();
+    @Override
+    public Boolean CheckWinner(Board board, Move move) {
+       // int  row = move.getCell().getRow();
+         int col = move.getCell().getCol();
+        Symbol symbol = move.getPlayer().getSymbol();
+
+        if(!colCounts.containsKey(col)){
+            colCounts.put(col,new HashMap<>());
+        }
+
+        Map<Symbol,Integer> rowMap = colCounts.get(col);
+
+        if(!rowMap.containsKey(symbol)){
+            rowMap.put(symbol,0);
+        }
+        rowMap.put(symbol,rowMap.get(symbol)+1);
+
+
+        return rowMap.get(symbol).equals(board.getDimension());
+
+    }
+}
 }
